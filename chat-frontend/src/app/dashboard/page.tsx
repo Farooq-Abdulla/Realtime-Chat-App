1afc3299b7fe14c95a65c529955de4b86a740482
Farooq Abdulla Shaik <farooqabdulla279@gmail.com>
2024-08-26 01:20:06 -0500

    feat: Dark-mode

--- chat-frontend/src/app/dashboard/page.tsx
+++ chat-frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import ChatWindow from "@/components/ui/chat-window";
 import SidebarDemo from "@/components/ui/sidebar-demo";
 import { cn } from "@/lib/utils";
 
@@ -5,7 +6,7 @@ export default function Dashboard() {
     return (
         <div
             className={cn(
-                "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+                "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
                 "h-screen"
             )}
         >
@@ -28,64 +29,9 @@ export default function Dashboard() {
                             ></div>
                         ))}
                     </div> */}
-
+                    <ChatWindow/>
                 </div>
             </div>
         </div>
     );
 };
-
-
-// "use client"
-
-// import { CornerDownLeft, Mic, Paperclip } from "lucide-react"
-
-// import { Button } from "@/components/ui/button"
-// import { Label } from "@/components/ui/label"
-// import { Textarea } from "@/components/ui/textarea"
-// import {
-//   Tooltip,
-//   TooltipContent,
-//   TooltipTrigger,
-// } from "@/components/ui/tooltip"
-
-// export default function ChatWindow() {
-//   return (
-//     <form
-//       className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
-//     >
-//       <Label htmlFor="message" className="sr-only">
-//         Message
-//       </Label>
-//       <Textarea
-//         id="message"
-//         placeholder="Type your message here..."
-//         className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
-//       />
-//       <div className="flex items-center p-3 pt-0">
-//         <Tooltip>
-//           <TooltipTrigger asChild>
-//             <Button variant="ghost" size="icon">
-//               <Paperclip className="size-4" />
-//               <span className="sr-only">Attach file</span>
-//             </Button>
-//           </TooltipTrigger>
-//           <TooltipContent side="top">Attach File</TooltipContent>
-//         </Tooltip>
-//         <Tooltip>
-//           <TooltipTrigger asChild>
-//             <Button variant="ghost" size="icon">
-//               <Mic className="size-4" />
-//               <span className="sr-only">Use Microphone</span>
-//             </Button>
-//           </TooltipTrigger>
-//           <TooltipContent side="top">Use Microphone</TooltipContent>
-//         </Tooltip>
-//         <Button type="submit" size="sm" className="ml-auto gap-1.5">
-//           Send Message
-//           <CornerDownLeft className="size-3.5" />
-//         </Button>
-//       </div>
-//     </form>
-//   )
-// }
--- chat-frontend/src/app/page.tsx
+++ chat-frontend/src/app/page.tsx
@@ -5,9 +5,10 @@ import { ChevronRight } from "lucide-react";
 
 export default async function Home() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
+    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
   <div className="text-center">
-    <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto relative z-20 py-1 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+    <h1 className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
       Experience <Cover>blazing-fast</Cover> <br/> real-time chat
     </h1>
     <p className="text-lg md:text-lg lg:text-xl font-semibold max-w-7xl mx-auto mt-2 mb-1 relative z-20 py-1 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">Log in to start connecting instantly!</p>
@@ -24,4 +25,5 @@ export default async function Home() {
 </div>
 
   );
+
 }
